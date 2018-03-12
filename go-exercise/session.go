77a98d01b9bcd522d5482ce0a87aa4998e2f2657
Ricky <Hello@Ricky.local>
2018-03-12 23:54:24 +0800

    模拟整个登录过程

--- go-exercise/session.go
+++ go-exercise/session.go
@@ -2,15 +2,23 @@ package exercise
 
 import (
 	"net/http"
-	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"encoding/json"
+	"path/filepath"
 	"fmt"
+	"time"
+	"io"
+	"strings"
+	"strconv"
+	"io/ioutil"
+
+	"github.com/juju/persistent-cookiejar"
 )
 
 type Auth struct {
-	Account string
-	Password string
+	Account string `json:"account"`
+	Password string `json:"password"`
 
 	loginType string
 	loginURL string
@@ -19,6 +27,13 @@ type Auth struct {
 type Session struct {
 	auth *Auth
 	client *http.Client
+}
+
+type loginResult struct {
+	R         int         `json:"r"`
+	Msg       string      `json:"msg"`
+	ErrorCode int         `json:"errcode"`
+	Data      interface{} `json:"data"`
 }
 
 var (
@@ -53,17 +68,73 @@ func (s *Session) LoadConfig(cfg string) {
 		panic("解析配置文件出错: " + err.Error())
 	}
 	s.auth = auth
-	fmt.Println(auth)
 }
 
 // Login 登录并保存 cookies
-func (s *Session) Login() {
+func (s *Session) Login() error {
 	if s.authenticated() {
 		logger.Success("已经是登录状态, 不需要重复登录")
-		//return nil
+		return nil
 	}
 
-	s.buildLoginForm()
+	// _xsrf=&captcha=VRKM&email=rickycxj%40gmail.com&password=test123&remember_me=true
+	form := s.buildLoginForm().Encode()
+	body := strings.NewReader(form)
+	req, err := http.NewRequest("POST", s.auth.loginURL, body)
+	if err != nil {
+		logger.Error("构造登录请求失败：%s", err.Error())
+		return err
+	}
+
+	headers := newHTTPHeaders(true)
+	headers.Set("Content-Length", strconv.Itoa(len(form)))
+	headers.Set("Content-Type", "application/x-www-form-urlencoded")
+	headers.Set("Referer", baseZhihuURL)
+	req.Header = headers
+
+	logger.Info("登录中，用户名： %s", s.auth.Account)
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		logger.Error("登录失败: %s", err.Error())
+		return err
+	}
+
+	logger.Info("content-type: ", resp.Header.Get("Content-Type"))
+
+	if strings.ToLower(resp.Header.Get("Content-Type")) != "application/json" {
+		logger.Error("服务器没有返回 json 数据")
+		return fmt.Errorf("未知的 Content-Type: %s", resp.Header.Get("Content-Type"))
+	}
+
+	defer resp.Body.Close()
+
+	result := loginResult{}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("读取响应内容失败: %s", err.Error())
+	}
+	logger.Info("登录响应内容： %s", strings.Replace(string(content), "\n", "", -1))
+
+	err = json.Unmarshal(content, &result)
+	if err != nil {
+		logger.Error("JSON解析失败：%s", err.Error())
+		return err
+	}
+
+	if result.R == 0 {
+		logger.Success("登录成功! ")
+		s.client.Jar.(*cookiejar.Jar).Save()
+		return nil
+	}
+
+	if result.R == 1 {
+		logger.Warn("登录失败! 原因：%s", result.Msg)
+		return fmt.Errorf("登录失败! 原因： %s", result.Msg)
+	}
+
+	logger.Error("登录出现未知错误： %s", string(content))
+	return fmt.Errorf("登录失败，未知错误：%s", string(content))
 }
 
 // authenticated 检查是否已经登录 (cookies 没有失效)
@@ -92,19 +163,91 @@ func (s *Session) Get(url string) (*http.Response, error) {
 	return s.client.Do(req)
 }
 
-func (s *Session) buildLoginForm() {
-	s.auth.toForm()
+func (s *Session) buildLoginForm() url.Values {
+	values := s.auth.toForm()
+	values.Set("_xsrf", s.searchXSRF())
+	values.Set("captcha", s.downloadCaptcha())
+	return values
 }
 
-func (auth *Auth) toForm() {
-	fmt.Println("test...", auth.isEmail())
+func (auth *Auth) toForm() url.Values {
+	if auth.isEmail() {
+		auth.loginType = "email"
+		auth.loginURL = makeZhihuLink("/login/email")
+	} else if auth.isPhone() {
+		auth.loginType = "phone_num"
+		auth.loginURL = makeZhihuLink("/login/phone_num")
+	} else {
+		panic("无法判断登录类型：" + auth.Account)
+	}
+	values := url.Values{}
+	logger.Info("登录类型： %s, 登录地址：%s", auth.loginType, auth.loginURL)
+	values.Set(auth.loginType, auth.Account)
+	values.Set("password", auth.Password)
+	values.Set("remember_me", "true")
+	return values
 }
 
 func (auth *Auth) isEmail() bool {
 	return isEmail(auth.Account)
 }
 
+func (auth *Auth) isPhone() bool {
+	return isPhone(auth.Account)
+}
 
+func (s *Session) searchXSRF() string {
+	resp, err := s.Get(baseZhihuURL)
+	if err != nil {
+		panic("获取 _xsrf失败：" + err.Error())
+	}
+
+	// retrieve from cookies
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "_xsrf" {
+			logger.Info("_xsrf: ", cookie.Value)
+			return cookie.Value
+		}
+	}
+	return ""
+}
+
+func (s *Session) downloadCaptcha() string {
+	url := makeZhihuLink(fmt.Sprintf("/captcha.gif?r=%d&type=login", 1000*time.Now().Unix()))
+	logger.Info("获取验证码： %s", url)
+	resp, err := s.Get(url)
+	if err != nil {
+		panic("获取验证码失败：" + err.Error())
+	}
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("获取验证码失败，StatusCode = %d", resp.StatusCode))
+	}
+
+	defer resp.Body.Close()
+
+	fileExt := strings.Split(resp.Header.Get("Content-Type"), "/")[1]
+	verifyImg := filepath.Join(getCwd(), "verify."+fileExt)
+	fd, err := os.OpenFile(verifyImg, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		panic("打开验证码文件失败：" + err.Error())
+	}
+	defer fd.Close()
+
+	io.Copy(fd, resp.Body) //保存验证码文件
+	openCaptchaFile(verifyImg)
+	captcha := readCaptchaInput()
+
+	return captcha
+}
+
+func getCwd() string{
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic("获取CWD失败：" + err.Error())
+	}
+	logger.Info("cwd: ", cwd)
+	return cwd
+}
 
 
 
--- go-exercise/util.go
+++ go-exercise/util.go
@@ -4,6 +4,13 @@ import (
 	"strings"
 	"net/http"
 	"regexp"
+	"path/filepath"
+	"os"
+	"os/exec"
+	"runtime"
+	"fmt"
+	
+	"github.com/fatih/color"
 )
 
 const (
@@ -12,6 +19,7 @@ const (
 )
 
 var (
+	reIsPhone = regexp.MustCompile(`(13|14|15|17|18|19)[0-9]{9}`)
 	reIsEmail = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	logger = Logger{Enabled: true}
 )
@@ -47,3 +55,76 @@ func newHTTPHeaders(isXhr bool) http.Header {
 func isEmail(value string) bool {
 	return reIsEmail.MatchString(value)
 }
+
+func isPhone(value string) bool {
+	return reIsPhone.MatchString(value)
+}
+
+func openCaptchaFile(filename string) error {
+	logger.Info("调用外部程序渲染验证码......")
+	var args []string
+	switch runtime.GOOS {
+	case "linux":
+		args = []string{"xdg-open", filename}
+	case "darwin":
+		args = []string{"open", filename}
+	case "freebsd":
+		args = []string{"open", filename}
+	case "netbsd":
+		args = []string{"open", filename}
+	case "windows":
+		var (
+			cmd = "url.dll, FileProtocolHandler"
+			runDll32 = filepath.Join(os.Getenv("SYSTEMROOT"), "System32", "rundll32.exe")
+		)
+		args = []string{runDll32, cmd, filename}
+	default:
+		fmt.Printf("无法确定操作系统，请自行打开验证码 %s 文件，并输入验证码。", filename)
+	}
+
+	logger.Info("Command: %s", strings.Join(args, " "))
+
+	err := exec.Command(args[0], args[1:]...).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readCaptchaInput() string {
+	var captcha string
+	fmt.Print(color.CyanString("请输入验证码："))
+	fmt.Scanf("%s", &captcha)
+	return captcha
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
